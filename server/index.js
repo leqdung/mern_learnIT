9b80b6326fbf58888e0d8d9ed4086c10d943784d
df <df>
2023-07-22 17:07:10 +0700

    setting auth and singup singin

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./routes/auth')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./routes/auth');
+const { configDotenv } = require('dotenv');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://ledung:LeDung%4090@cluster0.veajk3q.mongodb.net/?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.veajk3q.mongodb.net/?retryWrites=true&w=majority`,
       {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
       }
-    )
-    console.log('Mongoodb connected')
+    );
+    console.log('Mongoodb connected');
   } catch (error) {
-    console.log(error.message)
-    process.exit(1)
+    console.log(error.message);
+    process.exit(1);
   }
-}
+};
 
-connectDB()
-const app = express()
+connectDB();
+const app = express();
 
-app.use('/api/auth', authRouter)
-
-const PORT = 5000
+app.use(express.json()); //để đọc bất cứ dữ liệu req gửi lên
+app.use('/api/auth', authRouter);
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server start on PORT: ${PORT}`)
-})
+  console.log(`Server start on PORT: ${PORT}`);
+});
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,28 +8,79 @@ const jwt = require('jsonwebtoken')
 router.post('/register', async (req, res) => {
   const { username, password } = req.body
   //simple validation
-  if (!username || !password) {
-    res
+  if (!username || !password)
+    return res
       .status(400)
       .json({ success: false, message: 'Missing username or Password' })
 
-    try {
-      //check for exiting user
-      const user = await User.findOne({ username })
-      if (user) {
-        return res
-          .status(400)
-          .json({ success: false, message: 'Username already taken' })
-      }
-      // all cool => chuyển user name và password thành mã hash
-      const hashedPassword = await argon2.hash(password)
-      const newUser = new User({ username, password: hashedPassword })
-      await newUser.save() //import new user to db
-      // return token
-      const accessToken = jwt.sign({ userId: newUser._id })
-    } catch (error) {}
-  }
+  try {
+    //check for exiting user
+    const user = await User.findOne({ username })
+    if (user)
+      return res
+        .status(400)
+        .json({ success: false, message: 'Username already taken' })
+
+    // all cool => chuyển user name và password thành mã hash
+    const hashedPassword = await argon2.hash(password)
+    const newUser = new User({ username, password: hashedPassword })
+    await newUser.save() //import new user to db
+
+    // return token
+    const accessToken = jwt.sign(
+      { userId: newUser._id },
+      process.env.ACCESS_TOKEN
+    )
+    res.json({
+      success: true,
+      message: 'User created successful',
+      accessToken,
+    })
+  } catch (error) {}
+  console.log(error)
+  res.status(500).json({ success: false, message: 'Internal verver error' })
 })
 // desc
 // access public
+
+/**
+ * login user
+ * public
+ *
+ */
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  //simple validation
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: 'Missing username and/or Password' })
+
+  try {
+    const user = await User.findOne({ username })
+    if (!user)
+      return res
+        .status(400)
+        .send({ success: false, message: 'Incorect username and/or password' })
+    //username found
+    const passwordValid = await argon2.verify(user.password, password)
+    if (!passwordValid)
+      return res
+        .status(400)
+        .send({ success: false, message: 'Incorect username and/or password' })
+
+    //all good
+    const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN)
+    res.json({
+      success: true,
+      message: 'User login successful',
+      accessToken,
+    })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ success: false, message: 'Incorect username add/or password' })
+  }
+})
 module.exports = router
