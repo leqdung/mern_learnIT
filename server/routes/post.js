5206fec3bf38b732200e3207137d8d84c32f3771
df <df>
2023-07-29 21:52:01 +0700

    Thuc hiện thay đổi nọi dung của post thong qua su kiem tra ten user va ten id cua post

--- server/routes/post.js
+++ server/routes/post.js
@@ -16,7 +16,7 @@ router.get(
     try {
       const posts = await Post.find({ user: req.userId }).populate('user', [
         'username',
-      ]) /**populate truong user, chi lay trường nào mà  muốn (username)*/
+      ]) /**populate hiện ra các trường, chi lay trường nào mà  muốn (username)*/
       res.json({ success: true, posts })
     } catch (error) {
       console.log(error)
@@ -33,7 +33,7 @@ router.get(
  */
 router.post(
   '/',
-  verifyToken /**dung bac bao ve de kiem tra truoc khi login, khi kiem tra dung xong bac bao ve moi goi next */,
+  verifyToken /**dung bac bảo vệ để kiểm tra  truoc khi login, khi kiem tra dung xong bac bao ve moi goi next */,
   async (req, res) => {
     const { title, description, url, status } = req.body
     console.log(req.body)
@@ -42,21 +42,72 @@ router.post(
       return res
         .status(400)
         .json({ success: false, message: 'Title is require' })
-    //have data
+    //đã có data
     try {
-      const newPost = new Post({
-        title,
-        description,
-        url: url.startsWith('https://') ? url : `https://${url}`,
+      let updatedPost = {
+        /**dùng let để có thể thay đổi */ title,
+        description: description || ' ',
+        url: (url.startsWith('https://') ? url : `https://${url}`) || ' ',
         status: status || 'TO LEARN',
+      }
+      const postUpdateCondition = {
+        _id: req.params.id,
         user: req.userId,
-      })
-      await newPost.save()
-      res.json({ success: true, message: 'Happy leaning', post: newPost })
+      } /**điều kiện là id của post, và userId phải đúng */
+      updatedPost = await Post.findOneAndUpdate(
+        postUpdateCondition /**điều kiện */,
+        updatedPost /**lấy dữ liệu này để update */,
+        {
+          new: true,
+        } /**Sau khi update xong sẽ return về post mới /ngược lại return post cũ */
+      )
+      /**Kiểm tra nếu không có post đấy hoặc không có người user không đúng */
+      if (!updatedPost)
+        return res.status(401).json({
+          success: false,
+          message: 'Post not found or user not authorized',
+        })
+      res.json(
+        /**update thanh cong */
+        status(200).json({
+          success: true,
+          message: 'Excellent process',
+          post: updatedPost,
+        })
+      )
     } catch (error) {
       console.log(error)
       res.status(500).json({ success: false, message: 'Interal server error' })
     }
   }
 )
+/**
+ * @router @PUT api/post
+ * @desc Update post
+ * @access Private
+ */
+
+router.put('/:id', verifyToken, async (req, res) => {
+  //Móc dữ liệu
+  const { title, description, url, status } = req.body
+  console.log(req.body)
+  //simple valid
+  if (!title)
+    return res.status(400).json({ success: false, message: 'Title is require' })
+  //have data
+  try {
+    const newPost = new Post({
+      title,
+      description,
+      url: url.startsWith('https://') ? url : `https://${url}`,
+      status: status || 'TO LEARN',
+      user: req.userId,
+    })
+    await newPost.save()
+    res.json({ success: true, message: 'Happy leaning', post: newPost })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ success: false, message: 'Interal server error' })
+  }
+})
 module.exports = router
