2ad7b15b47a7cedcf31ca5c459d84396b248081c
df <df>
2023-07-29 16:55:25 +0700

    in ra các post của 1 user sau khi đã đăng nhập thành công

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,9 +2,35 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 const verifyToken = require('../middleware/auth')
-// @router post
-//@desc
-// @access
+
+/**
+ * @router @get post
+ * @desc get post
+ * @access Private
+ *
+ */
+router.get(
+  '/',
+  verifyToken /**dung bac bao ve de kiem tra truoc khi login, khi kiem tra dung xong bac bao ve moi goi next */,
+  async (req, res) => {
+    try {
+      const posts = await Post.find({ user: req.userId }).populate('user', [
+        'username',
+      ]) /**populate truong user, chi lay trường nào mà  muốn (username)*/
+      res.json({ success: true, posts })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ success: false, message: 'Interal server error' })
+    }
+  }
+)
+/**
+ * @router post
+ * @desc
+ * @access
+ *
+ * login and authorization
+ */
 router.post(
   '/',
   verifyToken /**dung bac bao ve de kiem tra truoc khi login, khi kiem tra dung xong bac bao ve moi goi next */,
